ad2520bdc93cf4f4c0490b72ae8c612261de7e40
akhil601 <akhil@vassarlabs.com>
2023-02-17 15:48:31 +0530

    UI::added index.js node file and package file

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const path = require('path')
+/* 
+    Incase you are using mongodb atlas database uncomment below line
+    and replace "mongoAtlasUri" with your mongodb atlas uri.
+*/
+// mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'UI/')));
+
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'UI/index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
